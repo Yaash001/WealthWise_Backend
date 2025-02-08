af75728947b088c875d1b43d7b4006118f0a99b9
Parth Chavan <parthchavan653@gmail.com>
2025-02-08 19:50:19 +0530

    new

--- src/main/java/com/wealthfitness/wealthfitness/WealthfitnessApplication.java
+++ src/main/java/com/wealthfitness/wealthfitness/WealthfitnessApplication.java
@@ -1,9 +1,12 @@
 package com.wealthfitness.wealthfitness;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class WealthfitnessApplication {
 
 	public static void main(String[] args) {
