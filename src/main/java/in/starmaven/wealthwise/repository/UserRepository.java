2c7b8136486e9b2dd8227f250b11b486a2268d2f
Parth Chavan <parthchavan653@gmail.com>
2025-03-10 20:10:24 +0530

    sql table update

--- src/main/java/in/starmaven/wealthwise/repository/UserRepository.java
+++ src/main/java/in/starmaven/wealthwise/repository/UserRepository.java
@@ -1,10 +1,10 @@
-//package in.starmaven.wealthwise.repository;
-//
-//import in.starmaven.wealthwise.entity.User;
-//import org.springframework.data.jpa.repository.JpaRepository;//to do CRUD operations on user table
-//import org.springframework.stereotype.Repository;
-//import java.util.Optional;
-//
-//public interface UserRepository extends JpaRepository<User, Long> {
-//    Optional<User> findByEmail(String email);//finding by email
-//}
+package in.starmaven.wealthwise.repository;
+
+import in.starmaven.wealthwise.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;//to do CRUD operations on user table
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+   Optional<User> findByEmail(String email);//finding by email
+}
--- src/main/java/in/starmaven/wealthwise/service/UserService.java
+++ src/main/java/in/starmaven/wealthwise/service/UserService.java
@@ -1,25 +1,25 @@
-//package in.starmaven.wealthwise.service;
-//
-//import in.starmaven.wealthwise.entity.User;
-//import in.starmaven.wealthwise.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Optional;
-//
-//@Service
-//public class UserService {
-//
-//    @Autowired
-//    private UserRepository userRepository;      //to access userRepository class
-//
-//    //saves user in database
-//    public User registerUser(User user) {
-//        return userRepository.save(user);
-//    }
-//
-//    //fetch by email
-//    public Optional<User> getUserByEmail(String email) {
-//        return userRepository.findByEmail(email);
-//    }
-//}
+package in.starmaven.wealthwise.service;
+
+import in.starmaven.wealthwise.entity.User;
+import in.starmaven.wealthwise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+   @Autowired
+   private UserRepository userRepository;      //to access userRepository class
+
+   //saves user in database
+   public User registerUser(User user) {
+       return userRepository.save(user);
+   }
+
+   //fetch by email
+   public Optional<User> getUserByEmail(String email) {
+       return userRepository.findByEmail(email);
+   }
+}
