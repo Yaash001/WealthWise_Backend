331cd1a347212631780ba0c954ee5a235dddcbe4
Yash <heyyaash01@gmail.com>
2025-05-31 20:25:32 +0530

    just checking github connection

--- src/main/java/in/starmaven/wealthwise/config/DatabaseConfig.java
+++ src/main/java/in/starmaven/wealthwise/config/DatabaseConfig.java
@@ -15,7 +15,7 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-
+ this is test
 import java.util.Properties;
 
 @Configuration
