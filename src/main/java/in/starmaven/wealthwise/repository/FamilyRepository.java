efd6e852ca5de2d35eacbf1002ab360fea275cef
Parth Chavan <parthchavan653@gmail.com>
2025-06-04 15:35:46 +0530

    .

--- src/main/java/in/starmaven/wealthwise/repository/FamilyRepository.java
+++
@@ -1,9 +0,0 @@
-package in.starmaven.wealthwise.repository;
-
-import in.starmaven.wealthwise.entity.Family;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface FamilyRepository extends MongoRepository<Family, String> {
-    Optional<Family> findByName(String familyName);
-}
--- src/main/java/in/starmaven/wealthwise/service/FamilyService.java
+++
@@ -1,7 +0,0 @@
-package in.starmaven.wealthwise.service;
-
-import in.starmaven.wealthwise.entity.Family;
-
-public interface FamilyService {
-    Family save(Family family);
-}
